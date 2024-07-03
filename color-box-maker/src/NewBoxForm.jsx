b6b08ea8b6091ea87699cefa32bb0b4f76c83fbb
Kate Vong <katiejnete@gmail.com>
2024-07-02 17:03:23 -0700

    complete color box maker with validation

--- color-box-maker/src/NewBoxForm.jsx
+++ color-box-maker/src/NewBoxForm.jsx
@@ -1,55 +1,60 @@
 import React, {useState} from "react";
-import {Formik, Form, Field} from 'formik';
+import {useFormik} from "formik";
+import './NewBoxForm.css'
 
 const NewBoxForm = ({addBox}) => {
-const INITIAL_STATE = {
-    backgroundColor: "",
-    width: "",
-    height: ""    
-};
-const [formData, setFormData] = useState(INITIAL_STATE);
-const handleChange = (evt) => {
-    const {name, value} = evt.target;
-    setFormData(fData => ({
-        ...fData,
-        [name]: value
-    }));
-}
-const handleSubmit = (evt) => {
-    evt.preventDefault();
-    addBox(formData);
-    setFormData(INITIAL_STATE);
-};
-
-return (
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="backgroundColor">Background color:</label>
-        <input 
-        id="backgroundColor"
-        type="text"
-        name="backgroundColor"
-        value={formData.backgroundColor}
-        onChange={handleChange}
-         />
-        <label htmlFor="width">Width:</label>         
-        <input 
-        id="width"
-        type="text"
-        name="width"
-        value={formData.width}
-        onChange={handleChange}
-         />
-        <label htmlFor="height">Height:</label>         
-        <input 
-        id="height"
-        type="text"
-        name="height"
-        value={formData.height}
-        onChange={handleChange}
-         />
-        <button>Add box</button>
-    </form>
-)
+    const formik = useFormik({
+        initialValues: { backgroundColor: "", width: "", height: "" },
+        onSubmit: (values, {resetForm}) => {
+            addBox(values);
+            resetForm();
+        },
+        validate: (values) => {
+            const errors = {};
+            if (!values.backgroundColor) errors.backgroundColor = 'Required';
+            if (!values.width) errors.width = 'Required';
+            if (!values.height) errors.height = 'Required';
+            return errors;
+        }
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="backgroundColor">Background color:</label>
+            <input 
+            id="backgroundColor"
+            type="text"
+            name="backgroundColor"
+            value={formik.values.backgroundColor}
+            onChange={formik.handleChange}
+             />
+            {formik.errors.backgroundColor && formik.touched.backgroundColor && (
+                <div className="error-message">{formik.errors.backgroundColor}</div>
+            )}
+            <label htmlFor="width">Width:</label>         
+            <input 
+            id="width"
+            type="number"
+            name="width"
+            value={formik.values.width}
+            onChange={formik.handleChange}
+             />
+            {formik.errors.width && formik.touched.width && (
+                <div className="error-message">{formik.errors.width}</div>
+            )}             
+            <label htmlFor="height">Height:</label>         
+            <input 
+            id="height"
+            type="number"
+            name="height"
+            value={formik.values.height}
+            onChange={formik.handleChange}
+             />
+            {formik.errors.height && formik.touched.height && (
+                <div className="error-message">{formik.errors.height}</div>
+            )}             
+            <button>Add box</button>
+        </form>
+    );
 };
 
 export default NewBoxForm;
